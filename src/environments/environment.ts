9111133bc10dbdd936cada55b20bbd7f16a1dfaf
facundo000 <facundo.guzman@mi.unc.edu.ar>
2025-03-26 09:58:59 -0300

    commit for production

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  api: "http://localhost:3000/api/v1/mortalkombat",
+  //     joseperez@mail.com
+  // cmCN7PWyaAEUH3#
+  auth0: {
+    domain: '',
+    clientId: '',
+    audience: ''
+  }
+};
